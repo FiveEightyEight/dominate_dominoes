b68ac74bc66c11fa0cfb102be80cec15c1eba8b5
Robert Abreu <robert.abreu3@gmail.com>
2023-04-13 18:21:11 -0400

    changes html to follow new layout based on design. Tested dominoes rendering

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,7 +1,20 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var Domino = /** @class */ (function () {
     function Domino(sideA, sideB) {
         this.sideA = sideA;
         this.sideB = sideB;
+        this.value = sideA + sideB;
+        this.id = "".concat(sideA, "|").concat(sideB);
     }
     Domino.prototype.getSideA = function () {
         return this.sideA;
@@ -9,14 +22,100 @@ var Domino = /** @class */ (function () {
     Domino.prototype.getSideB = function () {
         return this.sideB;
     };
+    Domino.prototype.getValue = function () {
+        return this.value;
+    };
     Domino.prototype.isDouble = function () {
         return this.sideA === this.sideB;
     };
     Domino.prototype.toString = function () {
         return "[".concat(this.sideA, "|").concat(this.sideB, "]");
     };
+    Domino.prototype.getID = function () {
+        return this.id;
+    };
     return Domino;
 }());
+var Team = /** @class */ (function () {
+    function Team(player1, player2) {
+        this.players = [player1, player2];
+    }
+    Team.prototype.getPlayers = function () {
+        return this.players;
+    };
+    return Team;
+}());
+var Player = /** @class */ (function () {
+    function Player(id, team) {
+        this.id = id;
+        this.moves = [];
+        this.team = team;
+    }
+    Player.prototype.getId = function () {
+        return this.id;
+    };
+    Player.prototype.getMoves = function () {
+        return this.moves;
+    };
+    Player.prototype.getTeam = function () {
+        return this.team;
+    };
+    Player.prototype.play = function (domino) {
+        this.moves.push(domino);
+    };
+    return Player;
+}());
+var Table = /** @class */ (function () {
+    function Table() {
+        this.table = [];
+    }
+    Table.prototype.add = function (domino, player) {
+        var sideA = domino.getSideA();
+        var sideB = domino.getSideB();
+        var newTile = {
+            domino: domino,
+            player: player,
+            position: [sideA, sideB],
+            id: domino.getID(),
+            value: domino.getValue()
+        };
+        if (this.table.length === 0) {
+            this.table.push(newTile);
+            return;
+        }
+        var firstTile = this.table[0];
+        var lastTile = this.table[this.table.length - 1];
+        if (firstTile.position[0] === sideA) {
+            this.table.unshift(newTile);
+            return;
+        }
+        else if (firstTile.position[0] === sideB) {
+            this.table.unshift(__assign(__assign({}, newTile), { position: [sideB, sideA] }));
+            return;
+        }
+        else if (lastTile.position[1] === sideA) {
+            this.table.push(newTile);
+            return;
+        }
+        else if (lastTile.position[1] === sideB) {
+            this.table.push(__assign(__assign({}, newTile), { position: [sideB, sideA] }));
+            return;
+        }
+    };
+    Table.prototype.getTableValue = function () {
+        return this.table.reduce(function (acc, tile) {
+            return acc + tile.value;
+        }, 0);
+    };
+    return Table;
+}());
+// Create teams and players
+// const team1: Team = new Team(new Player(1, team1), new Player(3, team1));
+// const team2: Team = new Team(new Player(2, team2), new Player(4, team2));
+// const players: Player[] = [...team1.getPlayers(), ...team2.getPlayers()];
+// Separate variable to store Player 1's hand
+var player1Hand = [];
+var tileElements = {};
 var dominoes = [];
 for (var i = 0; i <= 6; i++) {
     for (var j = i; j <= 6; j++) {
@@ -24,8 +123,29 @@ for (var i = 0; i <= 6; i++) {
     }
 }
 // get element by class name domino-container
-var dominoContainer = document.querySelector('.domino-container');
+var dominoTable = document.querySelector('.top-left-2');
+var rightSide = document.querySelector('.right');
 var dominoesTiles = "";
+// dominoTable.innerHTML = dominoesTiles;
+// ------- GAME STATE --------->
+// type GameState = {
+//     rightSelectedTitle: string;
+// }
+// class GameState {
+//     rightSelectedTile: string;
+//     constructor() {
+//         this.rightSelectedTile = '';
+//     }
+//     setRightSelectedTile(title: string) {
+//         this.rightSelectedTile = title;
+//     }
+// }
+// const state: GameState = {
+//     rightSelectedTile: '',
+// };
+// function setGameState(state: GameState, key) {
+// }
+// ------- SETUP --------->
 for (var i = 0; i < dominoes.length; i++) {
     var sideA = dominoes[i].getSideA();
     var sideB = dominoes[i].getSideB();
@@ -33,6 +153,43 @@ for (var i = 0; i < dominoes.length; i++) {
     var bottomDots = sideB ? "dot-".concat(sideB) : '';
     var topDotList = sideA ? "<span class=\"dot\"></span>".repeat(sideA) : '';
     var bottomDotList = sideB ? "<span class=\"dot\"></span>".repeat(sideB) : '';
-    dominoesTiles += "\n        <div class=\"domino-tile\">\n            <div class=\"tile-top ".concat(topDots, "\">\n                ").concat(topDotList, "\n            </div>\n            <div class=\"tile-divider\"></div>\n            <div class=\"tile-bottom ").concat(bottomDots, "\">\n                ").concat(bottomDotList, "\n            </div>\n        </div>\n    ");
+    var id = dominoes[i].getID();
+    tileElements[id] = "\n        <div class=\"domino-tile\" data-id=".concat(id, ">\n            <div class=\"tile-top ").concat(topDots, "\">\n                ").concat(topDotList, "\n            </div>\n            <div class=\"tile-divider\"></div>\n            <div class=\"tile-bottom ").concat(bottomDots, "\">\n                ").concat(bottomDotList, "\n            </div>\n        </div>\n    ");
 }
-dominoContainer.innerHTML = dominoesTiles;
+rightSide === null || rightSide === void 0 ? void 0 : rightSide.addEventListener('click', function (e) {
+    var target = e.target;
+    var dominoTile = target.closest('.domino-tile');
+    if (dominoTile) {
+        if (dominoTile.classList.contains('selected')) {
+            dominoTile.classList.remove('selected');
+        }
+        else {
+            dominoTile.classList.add('selected');
+        }
+    }
+});
+function renderRightSide() {
+    if (!rightSide)
+        return;
+    var _loop_1 = function (i) {
+        var rowToRender = dominoes.filter(function (domino) { return domino.getSideB() === i; });
+        for (var j = 0; j < rowToRender.length; j++) {
+            var sideA = rowToRender[j].getSideA();
+            var sideB = rowToRender[j].getSideB();
+            var topDots = sideA ? "dot-".concat(sideA) : '';
+            var bottomDots = sideB ? "dot-".concat(sideB) : '';
+            var topDotList = sideA ? "<span class=\"dot\"></span>".repeat(sideA) : '';
+            var bottomDotList = sideB ? "<span class=\"dot\"></span>".repeat(sideB) : '';
+            var id = rowToRender[j].getID();
+            rightSide.innerHTML += "\n            <div class=\"domino-tile\" data-id=".concat(id, ">\n                <div class=\"tile-top ").concat(topDots, "\">\n                    ").concat(topDotList, "\n                </div>\n                <div class=\"tile-divider\"></div>\n                <div class=\"tile-bottom ").concat(bottomDots, "\">\n                    ").concat(bottomDotList, "\n                </div>\n            </div>\n            ");
+            // Add buffers at after these index 0, 2, 5, 9, 14, 20,
+        }
+        var row = rowToRender.length;
+        // if (row < 7) {
+        rightSide.innerHTML += "\n            <div id=\"cell-".concat(row, "\">\n            </div>\n        ");
+    };
+    for (var i = 0; i < 7; i++) {
+        _loop_1(i);
+    }
+}
+renderRightSide();
--- src/logic/index.ts
+++ src/logic/index.ts
@@ -2,11 +2,13 @@ class Domino {
     private sideA: number;
     private sideB: number;
     private value: number;
+    private id: string;
 
     constructor(sideA: number, sideB: number) {
         this.sideA = sideA;
         this.sideB = sideB;
         this.value = sideA + sideB;
+        this.id = `${sideA}|${sideB}`;
     }
 
     getSideA(): number {
@@ -28,21 +30,167 @@ class Domino {
     toString(): string {
         return `[${this.sideA}|${this.sideB}]`;
     }
+    getID(): string {
+        return this.id;
+    }
 }
 
+class Team {
+    private players: Player[];
+
+    constructor(player1: Player, player2: Player) {
+        this.players = [player1, player2];
+    }
+
+    getPlayers(): Player[] {
+        return this.players;
+    }
+}
+
+class Player {
+    private id: number;
+    private moves: Domino[];
+    private team: Team;
+
+    constructor(id: number, team: Team) {
+        this.id = id;
+        this.moves = [];
+        this.team = team;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getMoves(): Domino[] {
+        return this.moves;
+    }
+
+    getTeam(): Team {
+        return this.team;
+    }
+
+    play(domino: Domino): void {
+        this.moves.push(domino);
+    }
+
+    // predictOpponentMoves(remainingDominoes: Domino[], currentPlayerHand: Domino[]): Domino[] {
+    // Implement prediction logic based on the player's move history
+    // }
+}
+
+type TableDomino = {
+    domino: Domino;
+    player: Player;
+    //           [played, in play] 
+    // position: [number, number];
+    position: number[];
+    id: string;
+    value: number;
+}
+class Table {
+    private table: TableDomino[];
+    constructor() {
+        this.table = [];
+    }
+
+    add(domino: Domino, player: Player): void {
+        const sideA: number = domino.getSideA();
+        const sideB: number = domino.getSideB();
+        const newTile: TableDomino = {
+            domino: domino,
+            player: player,
+            position: [sideA, sideB],
+            id: domino.getID(),
+            value: domino.getValue()
+        }
+        if (this.table.length === 0) {
+            this.table.push(newTile);
+            return;
+        }
+        const firstTile: TableDomino = this.table[0];
+        const lastTile: TableDomino = this.table[this.table.length - 1];
+
+        if (firstTile.position[0] === sideA) {
+            this.table.unshift(newTile);
+            return;
+        } else if (firstTile.position[0] === sideB) {
+            this.table.unshift({
+                ...newTile,
+                position: [sideB, sideA]
+            });
+            return;
+        } else if (lastTile.position[1] === sideA) {
+            this.table.push(newTile);
+            return;
+        } else if (lastTile.position[1] === sideB) {
+            this.table.push({
+                ...newTile,
+                position: [sideB, sideA]
+            });
+            return;
+        }
+    }
+
+    getTableValue(): number {
+        return this.table.reduce((acc, tile) => {
+            return acc + tile.value;
+        }, 0);
+    }
+
+}
+
+// Create teams and players
+// const team1: Team = new Team(new Player(1, team1), new Player(3, team1));
+// const team2: Team = new Team(new Player(2, team2), new Player(4, team2));
+
+// const players: Player[] = [...team1.getPlayers(), ...team2.getPlayers()];
+
+// Separate variable to store Player 1's hand
+const player1Hand: Domino[] = [];
+type TilesElements = {
+    [key: string]: string;
+}
+const tileElements: TilesElements = {};
 const dominoes: Domino[] = [];
 
 for (let i = 0; i <= 6; i++) {
-  for (let j = i; j <= 6; j++) {
-    dominoes.push(new Domino(i, j));
-  }
+    for (let j = i; j <= 6; j++) {
+        dominoes.push(new Domino(i, j));
+    }
 }
 
 
 // get element by class name domino-container
-const dominoContainer = document.querySelector('.domino-container');
+const dominoTable = document.querySelector('.top-left-2');
+const rightSide = document.querySelector('.right');
 let dominoesTiles = ``;
+// dominoTable.innerHTML = dominoesTiles;
 
+// ------- GAME STATE --------->
+// type GameState = {
+//     rightSelectedTitle: string;
+// }
+// class GameState {
+//     rightSelectedTile: string;
+//     constructor() {
+//         this.rightSelectedTile = '';
+//     }
+
+//     setRightSelectedTile(title: string) {
+//         this.rightSelectedTile = title;
+//     }
+// }
+// const state: GameState = {
+//     rightSelectedTile: '',
+
+// };
+
+// function setGameState(state: GameState, key) {
+
+// }
+
+// ------- SETUP --------->
 for (let i = 0; i < dominoes.length; i++) {
     const sideA = dominoes[i].getSideA();
     const sideB = dominoes[i].getSideB();
@@ -50,8 +198,9 @@ for (let i = 0; i < dominoes.length; i++) {
     const bottomDots = sideB ? `dot-${sideB}` : '';
     const topDotList = sideA ? `<span class="dot"></span>`.repeat(sideA) : '';
     const bottomDotList = sideB ? `<span class="dot"></span>`.repeat(sideB) : '';
-  dominoesTiles += `
-        <div class="domino-tile">
+    const id = dominoes[i].getID();
+    tileElements[id] = `
+        <div class="domino-tile" data-id=${id}>
             <div class="tile-top ${topDots}">
                 ${topDotList}
             </div>
@@ -63,4 +212,49 @@ for (let i = 0; i < dominoes.length; i++) {
     `;
 }
 
-dominoContainer.innerHTML = dominoesTiles;
+rightSide?.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+    const dominoTile = target.closest('.domino-tile');
+    if (dominoTile) {
+        if (dominoTile.classList.contains('selected')) {
+            dominoTile.classList.remove('selected')
+        } else {
+            dominoTile.classList.add('selected')
+        }
+    }
+});
+
+function renderRightSide() {
+    if (!rightSide) return;
+    for (let i = 0; i < 7; i++) {
+        const rowToRender = dominoes.filter((domino) => domino.getSideB() === i);
+        for (let j = 0; j < rowToRender.length; j++) {
+            const sideA = rowToRender[j].getSideA();
+            const sideB = rowToRender[j].getSideB();
+            const topDots = sideA ? `dot-${sideA}` : '';
+            const bottomDots = sideB ? `dot-${sideB}` : '';
+            const topDotList = sideA ? `<span class="dot"></span>`.repeat(sideA) : '';
+            const bottomDotList = sideB ? `<span class="dot"></span>`.repeat(sideB) : '';
+            const id = rowToRender[j].getID();
+
+            rightSide.innerHTML += `
+            <div class="domino-tile" data-id=${id}>
+                <div class="tile-top ${topDots}">
+                    ${topDotList}
+                </div>
+                <div class="tile-divider"></div>
+                <div class="tile-bottom ${bottomDots}">
+                    ${bottomDotList}
+                </div>
+            </div>
+            `;
+        }
+        const row = rowToRender.length;
+        rightSide.innerHTML += `
+            <div id="cell-${row}">
+            </div>
+        `;
+
+    }
+}
+renderRightSide();
